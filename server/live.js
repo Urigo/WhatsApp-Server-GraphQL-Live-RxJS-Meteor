9cb28caedb1ea1730b14100bdc40d59eb525b4c5
Niccolò Belli <niccolo.belli@linuxsystems.it>
2017-10-24 12:48:23 +0200

    Add live

--- server/live.js
+++ server/live.js
@@ -1,20 +1,55 @@
 import { makeExecutableSchema } from 'graphql-tools';
+import { MongoObservable } from 'meteor-rxjs';
 import { runGraphQLServer } from 'meteor-graphql-rxjs';
 
-import schema from '../imports/graphql/schema';
-import resolvers from '../imports/graphql/resolvers';
+const Messages = new MongoObservable.Collection('my-messages');
 
-const executableSchema = makeExecutableSchema({
-    typeDefs: [schema],
-    resolvers: resolvers
+Messages.remove({});
+
+Messages.insert({content: "1"});
+Messages.insert({content: "2"});
+Messages.insert({content: "3"});
+
+for (let i = 1; i <= 1000; i++) {
+    Meteor.setTimeout(() => {
+        Messages.insert({content: i+3});
+    }, i * 5000);
+}
+
+const schema = makeExecutableSchema({
+    typeDefs: `
+  type Message {
+    content: String
+  }
+
+  type Query {
+    allMessages: [Message]
+  }
+  `,
+    resolvers: {
+        Query: {
+            allMessages: (root, args, ctx) => {
+                return ctx.Messages.find();
+            },
+        },
+    },
 });
+
+const defaultQuery = `
+  query {
+    allMessages @live {
+      content
+    }
+  }
+`;
 
 Meteor.startup(() => {
     const sub = runGraphQLServer(Npm.require, {
-        schema: executableSchema,
+        schema,
         graphiql: true,
+        graphiqlQuery: defaultQuery,
         createContext: (payload) => ({
-
+            Messages
         }),
     })
         .subscribe(undefined, (error) => {
